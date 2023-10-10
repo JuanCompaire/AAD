f40bd3e6846df69ed09930fe9d2a2756ce1d756c
JuanCompaire <juancompaire2004@gmail.com>
2023-10-10 13:11:46 +0200

    razor pages AAD, mirando el codigo
    Signed-off-by: JuanCompaire <juancompaire2004@gmail.com>

---
+++ RazorPages/RazorPages/Pages/Alumnos/Index.cshtml.cs
@@ -0,0 +1,20 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace RazorPages.Pages.Alumnos
+
+
+{
+    public class IndexModel : PageModel
+
+        
+    {
+		public string mensaje { get; set; }
+
+        public void OnGet()
+        {
+			mensaje = "Hola a todos los alumnos";
+
+		}
+	}
+}
