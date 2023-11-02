1218b62baf07404b37de140757198d9947a62556
JuanCompaire <juancompaire2004@gmail.com>
2023-11-02 17:56:47 +0100

    metemos la base de datos
    Signed-off-by: JuanCompaire <juancompaire2004@gmail.com>

---
+++ RazorPages/RazorPages.Services/AlumnoDbContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using RazorPages.Modelos;
+
+namespace RazorPages.Services
+{
+	public class AlumnoDbContext : DbContext
+	{
+		public AlumnoDbContext(DbContextOptions<AlumnoDbContext> options) : base(options)
+		{
+		}
+        public DbSet<Alumno> Alumnos { get; set; }
+    }
+}
--- RazorPages/RazorPages/Program.cs
+++ RazorPages/RazorPages/Program.cs
@@ -1,3 +1,4 @@
+using Microsoft.EntityFrameworkCore;
 using RazorPages.Services;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -5,6 +6,14 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddRazorPages();
 builder.Services.AddSingleton<IAlumnoRepositorio, AlumnoRepositorio>();
+
+IConfiguration configuration = new ConfigurationBuilder()
+	.AddJsonFile("appsettings.json")
+	.AddEnvironmentVariables()
+	.Build();
+
+builder.Services.AddDbContextPool<AlumnoDbContext>(options => options.UseSqlServer(configuration.GetConnectionString("ColegioDBConnection"));
+
 
 var app = builder.Build();
 
